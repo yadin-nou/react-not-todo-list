92819701831f5a733235bd644e40174df1a88925
Yadin Nou <nouyadin@gmail.com>
2026-08-12 05:00:19 +1000

    changed code strure

--- src/components/TableList.jsx
+++ src/components/TableList.jsx
@@ -4,43 +4,50 @@ import { useState } from "react";
 
 const TableList = ({ id, taskList, switchList, deleteTask }) => {
   const [selDelete, setSelDelete] = useState([]);
-  // const [chED, setchED] = useState(false);
+  // const [badDelete, setbadDelete] = useState([]);
+  // const [entryDelete, setEntryDelete] = useState([]);
+
+  const entryList = taskList
+    .filter((item) => item.type === "entry")
+    .map((item) => item._id);
+  const badList = taskList
+    .filter((item) => item.type === "bad")
+    .map((item) => item._id);
+
   const handleSwitch = (id, type) => {
     switchList(id, type);
   };
 
   const handleSelect = (e) => {
     const { checked, value } = e.target;
-    //console.log(checked, value);
+
     if (checked) {
       if (value === "all-entryList") {
-        //  setchED(true);
-        //console.log(value, id);
-        const allID = taskList
-          .filter((item) => item.type === "entry")
-          .map((item) => item._id);
-        // setSelDelete([...selDelete, ...allID]);
-        setSelDelete(allID);
-        // console.log(allID, "---");
+        setSelDelete(...selDelete, entryList);
         return;
       }
+      if (value === "all-badList") {
+        setSelDelete(...selDelete, badList);
+
+        return;
+      }
+
+      //setSelDelete([...selDelete, value]);
       setSelDelete([...selDelete, value]);
     } else {
       if (value === "all-entryList") {
-        //setchED(false);
-        //console.log(value, id);
-        const entryID = taskList
-          .filter((item) => item.type === "entry")
-          .map((item) => item._id);
-        //console.log(entry, " IDentry");
-        setSelDelete(selDelete.filter((item) => !entryID.includes(item)));
+        setSelDelete(selDelete.filter((item) => !entryList.includes(item)));
         return;
       }
-      //console.log(checked, id);
+      if (value === "all-badList") {
+        setSelDelete(selDelete.filter((item) => !badList.includes(item)));
+        return;
+      }
       setSelDelete(selDelete.filter((_id) => _id !== value));
     }
   };
-  // console.log(selDelete);
+  console.log(selDelete);
+  //console.log("bad", badDelete);
   return (
     <>
       <input
@@ -54,66 +61,65 @@ const TableList = ({ id, taskList, switchList, deleteTask }) => {
       <table className="table table-striped table-hover border">
         <tbody id={id}>
           {id === "entryList" &&
-            taskList
-              .filter((item) => item.type === "entry")
-              .map((item, index) => (
-                <tr key={item?._id}>
-                  <td>{index + 1}</td>
-                  <td>
-                    <input
-                      className="form-check-input"
-                      type="checkbox"
-                      value={item?._id}
-                      checked={selDelete.includes(item?._id)}
-                      onChange={handleSelect}
-                    />{" "}
-                    {item.task}
-                  </td>
-                  <td>{item.hour} hr</td>
-                  <td className="text-end">
-                    <Button
-                      clColor="btn-danger"
-                      label={<i className="fa-solid fa-trash"></i>}
-                      fn={() => deleteTask(item?._id)}
-                    />
-                    <Button
-                      clColor="btn-success"
-                      label={<i className="fa-solid fa-arrow-right"></i>}
-                      fn={() => handleSwitch(item?._id, "bad")}
-                    />
-                  </td>
-                </tr>
-              ))}
+            entryList.map((item, index) => (
+              <tr key={item?._id}>
+                <td>{index + 1}</td>
+                <td>
+                  <input
+                    className="form-check-input"
+                    type="checkbox"
+                    value={item?._id}
+                    checked={selDelete.includes(item?._id)}
+                    id="entry"
+                    onChange={handleSelect}
+                  />{" "}
+                  {item.task}
+                </td>
+                <td>{item.hour} hr</td>
+                <td className="text-end">
+                  <Button
+                    clColor="btn-danger"
+                    label={<i className="fa-solid fa-trash"></i>}
+                    fn={() => deleteTask(item?._id)}
+                  />
+                  <Button
+                    clColor="btn-success"
+                    label={<i className="fa-solid fa-arrow-right"></i>}
+                    fn={() => handleSwitch(item?._id, "bad")}
+                  />
+                </td>
+              </tr>
+            ))}
           {id === "badList" &&
-            taskList
-              .filter((item) => item.type === "bad")
-              .map((item, index) => (
-                <tr key={item?._id}>
-                  <td>{index + 1}</td>
-                  <td>
-                    <input
-                      className="form-check-input"
-                      type="checkbox"
-                      value={item?._id}
-                      onChange={handleSelect}
-                    />{" "}
-                    {item.task}
-                  </td>
-                  <td>{item.hour} hr</td>
-                  <td className="text-end">
-                    <Button
-                      clColor="btn-warning"
-                      label={<i className="fa-solid fa-arrow-left"></i>}
-                      fn={() => handleSwitch(item._id, "entry")}
-                    />
-                    <Button
-                      clColor="btn-danger"
-                      label={<i className="fa-solid fa-trash"></i>}
-                      fn={() => deleteTask(item._id)}
-                    />
-                  </td>
-                </tr>
-              ))}
+            badList.map((item, index) => (
+              <tr key={item?._id}>
+                <td>{index + 1}</td>
+                <td>
+                  <input
+                    className="form-check-input"
+                    type="checkbox"
+                    value={item?._id}
+                    checked={selDelete.includes(item?._id)}
+                    onChange={handleSelect}
+                    id="bad"
+                  />{" "}
+                  {item.task}
+                </td>
+                <td>{item.hour} hr</td>
+                <td className="text-end">
+                  <Button
+                    clColor="btn-warning"
+                    label={<i className="fa-solid fa-arrow-left"></i>}
+                    fn={() => handleSwitch(item._id, "entry")}
+                  />
+                  <Button
+                    clColor="btn-danger"
+                    label={<i className="fa-solid fa-trash"></i>}
+                    fn={() => deleteTask(item._id)}
+                  />
+                </td>
+              </tr>
+            ))}
         </tbody>
       </table>
     </>
