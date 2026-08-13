4d0bfdad9d7245a16a2ee2c7889c64dd58a60975
Yadin Nou <nouyadin@gmail.com>
2026-08-14 07:49:44 +1000

    changed NODE_ENV

--- src/helpers/axiosHelper.js
+++ src/helpers/axiosHelper.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 //const urlEP = "http://localhost:8000/api/v1/tasks";
+// change url when deploy to the server hosting
 const urlEP = import.meta.env.NODE_ENV
   ? "/api/v1/tasks"
   : "http://localhost:8000/api/v1/tasks";
